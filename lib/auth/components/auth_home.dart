e272e089cea3bea481b9b152a9f4efbbdf7dc582
Mares1492 <msumarok@tlu.ee>
2023-05-03 22:54:26 +0300

    Update auth_home.dart
    - added anon user deletion on sign out
    - added error message on guest log in fail
    - added route to SignInRegister page

--- lib/auth/components/auth_home.dart
+++ lib/auth/components/auth_home.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../auth.dart';
+import 'sign_in_register.dart';
 
 class AuthHome extends StatefulWidget {
   const AuthHome({super.key});
@@ -12,6 +13,7 @@ class AuthHome extends StatefulWidget {
 
 class _AuthHomeState extends State<AuthHome> {
   User? _user = Auth().currentUser;
+  String? _errorMessage;
 
   _isAuth() {
     setState(() {
@@ -20,23 +22,76 @@ class _AuthHomeState extends State<AuthHome> {
   }
 
   Future<void> _handleAuthClick() async {
-    await Auth().signInAnonymously();
-    _isAuth();
+    try {
+      await Auth().signInAnonymously();
+      _isAuth();
+    } on FirebaseAuthException catch (e) {
+      setState(() {
+        _errorMessage = e.toString();
+      });
+    }
   }
 
   Future<void> _handleSignOutClick() async {
-    await Auth().signOut();
+    if (_user?.isAnonymous ?? true) {
+      await _user?.delete();
+    } else {
+      await Auth().signOut();
+    }
     _isAuth();
   }
 
-  Widget userUid() {
-    return Text(_user == null ? "Not signed in" : _user?.email ?? "Anonymous");
+  Widget _userUid() {
+    return Text(_user == null ? "Not signed in" : _user?.email ?? "Guest");
   }
 
-  Widget contextButton() {
+  Widget _errorMessageWidget() {
+    return Text(_errorMessage ?? "", style: const TextStyle(color: Colors.red));
+  }
+
+  Widget _contextButtonContent() {
     return ElevatedButton(
       onPressed: _user != null ? _handleSignOutClick : _handleAuthClick,
-      child: Text(_user != null ? "Sign Out" : "Sign In"),
+      child: Text(_user != null ? "Sign Out" : "Sign in guest"),
+    );
+  }
+
+  Widget _contextButtons() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _contextButtonContent(),
+        ElevatedButton(
+          onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const SignInRegister(),
+            ),
+          ),
+          child: const Text("Sign in/Register"),
+        ),
+      ],
+    );
+  }
+
+  Widget _contextButtonsContainer() {
+    return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 300.0,
+        minWidth: 200.0,
+        minHeight: 180.0,
+      ),
+      padding: const EdgeInsets.all(15.0),
+      decoration: const BoxDecoration(
+        color: Colors.white60,
+        border: Border.fromBorderSide(
+          BorderSide(
+            width: 1,
+            color: Colors.grey,
+          ),
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      child: _contextButtons(),
     );
   }
 
@@ -47,8 +102,9 @@ class _AuthHomeState extends State<AuthHome> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            userUid(),
-            contextButton(),
+            _userUid(),
+            _contextButtonsContainer(),
+            _errorMessageWidget(),
           ],
         ),
       ),
