d7a7060c6a8023b4484fe952a4a4bbb87bc0424c
Mares1492 <msumarok@tlu.ee>
2023-05-10 14:04:12 +0300

    Different adjustments to person.dart
    - users can be added and removed as friends
    - if user has no avatar, it will be generated based on name
    - avatars will have random bg color and change it after every render
    - contacts are now displayed if user has any
    - added specified callbacks dependent on the contact type
    - added loading screen and component to show that user is not found

--- lib/db/firebase/controllers/firebase_user_controller.dart
+++ lib/db/firebase/controllers/firebase_user_controller.dart
@@ -1,7 +1,44 @@
 import 'package:firebase_database/firebase_database.dart';
 
+import '../../../auth/auth.dart';
+
 class FirebaseUserController {
   DatabaseReference ref = FirebaseDatabase.instance.ref();
+
+  Future<bool> isFriend(String userID) async {
+    return await ref
+        .child('users/${Auth().currentUser!.uid}/friends')
+        .once()
+        .then((DatabaseEvent event) {
+      if (event.snapshot.value == null) {
+        return false;
+      }
+      Map<dynamic, dynamic> data = event.snapshot.value as Map;
+      List<String> keys = data.keys.cast<String>().toList();
+      for (String key in keys) {
+        if (key == userID) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
+
+  Future<void> addFriend(String userID) async {
+    final Map<String, bool> updates = {};
+    //TODO: when adding a friend, add value false that represents a pending friend request
+    //TODO: change on true when the friend accepts the request
+    updates['/users/${Auth().currentUser!.uid}/friends/$userID'] = true;
+    return FirebaseDatabase.instance.ref().update(updates);
+  }
+
+  Future<void> removeFriend(String userID) async {
+    final Map<String, dynamic> updates = {};
+
+    updates['/users/${Auth().currentUser!.uid}/friends/$userID'] = null;
+    return FirebaseDatabase.instance.ref().update(updates);
+  }
+
   Future<void> writeNewUser({
     required String uid,
     required String email,
--- lib/pages/person.dart
+++ lib/pages/person.dart
@@ -1,9 +1,14 @@
+import 'dart:math';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'friends.dart';
+import '../auth/auth.dart';
+import '../db/dto/person_dto.dart';
+import '../db/firebase/controllers/firebase_user_controller.dart';
 
 class Person extends StatefulWidget {
-  final Friend data;
-  const Person({super.key, required this.data});
+  final String authorId;
+  const Person({super.key, required this.authorId});
 
   @override
   State<Person> createState() => _PersonState();
@@ -12,10 +17,108 @@ class Person extends StatefulWidget {
 class _PersonState extends State<Person> {
   // * Friend status
   // TODO: Implement friend status check
-  bool _isFriend = true;
+  late final Query _dbRef;
+  late final PersonDTO? _personData;
+  bool _isFriend = false;
+  bool _hasData = false;
+  bool _userNotFound = false;
+  String? _errorMessage;
+  final List<String> colors = [
+    "F8B195",
+    "F67280",
+    "C06C84",
+    "6C5B7B",
+    "355C7D",
+    "99B898",
+    "FECEAB",
+    "FF847C",
+    "E84A5F",
+    "2A363B"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _dbRef = FirebaseDatabase.instance.ref().child('users/${widget.authorId}');
+    _dbRef.once().then((DatabaseEvent event) {
+      // * a bit lazy way to get single user data
+      if (event.snapshot.value != null) {
+        Map data = event.snapshot.value as Map;
+        data['key'] = event.snapshot.key;
+        _getIsFriend();
+        setState(() {
+          try {
+            _personData = PersonDTO.fromSnapshot(data);
+            _hasData = true;
+          } catch (e) {
+            setState(() {
+              _userNotFound = true;
+              _errorMessage = e.toString();
+            });
+          }
+        });
+      } else {
+        setState(() {
+          _userNotFound = true;
+        });
+      }
+    });
+  }
+
+  void _getIsFriend() async {
+    bool isFriend = await FirebaseUserController().isFriend(widget.authorId);
+    setState(() {
+      _isFriend = isFriend;
+    });
+  }
+
+  Widget _contacts() {
+    List<dynamic>? contacts = _personData?.contactList;
+    if (contacts == null) {
+      return const SizedBox.shrink();
+    }
+    return Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Contacts:',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            for (Map contact in contacts)
+              Padding(
+                padding: const EdgeInsets.only(top: 5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 5),
+                      child: contact['icon'],
+                    ),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      onPressed: () => contact['callback'](contact['value']),
+                      child: Text(contact['value']),
+                    ),
+                  ],
+                ),
+              ),
+          ],
+        ));
+  }
 
   // * Avatar
   Widget _avatar() {
+    String bgColor = colors[_getRandomNumber(0, colors.length)];
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20),
       child: Container(
@@ -23,7 +126,11 @@ class _PersonState extends State<Person> {
         height: 100,
         decoration: const BoxDecoration(shape: BoxShape.circle),
         child: CircleAvatar(
-          backgroundImage: NetworkImage(widget.data.avatar),
+          backgroundColor: bgColor.length == 6
+              ? Color(int.parse('0xFF$bgColor'))
+              : Colors.grey,
+          backgroundImage: NetworkImage(_personData?.avatar ??
+              "https://ui-avatars.com/api/?name=${_personData?.name}&background=$bgColor&size=128"),
         ),
       ),
     );
@@ -35,7 +142,7 @@ class _PersonState extends State<Person> {
       width: double.infinity,
       padding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
       child: Text(
-        widget.data.name,
+        _personData?.name ?? 'Name',
         textAlign: TextAlign.center,
         style: const TextStyle(
           fontSize: 26,
@@ -59,11 +166,11 @@ class _PersonState extends State<Person> {
 
 // * Bio
   Widget _bio() {
-    return const Padding(
-      padding: EdgeInsets.only(top: 10),
+    return Padding(
+      padding: const EdgeInsets.only(top: 10),
       child: Text(
-        "Bio template",
-        style: TextStyle(fontSize: 16),
+        _personData?.bio ?? 'Bio',
+        style: const TextStyle(fontSize: 16),
       ),
     );
   }
@@ -74,11 +181,7 @@ class _PersonState extends State<Person> {
       child: SizedBox(
         width: 170.0,
         child: ElevatedButton(
-          onPressed: () {
-            setState(() {
-              _isFriend = !_isFriend;
-            });
-          },
+          onPressed: _handleFriendButtonClick,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
@@ -120,12 +223,58 @@ class _PersonState extends State<Person> {
     );
   }
 
+  Widget _buildUserNotFound() {
+    return Column(
+      children: [
+        SizedBox(height: MediaQuery.of(context).size.height / 3),
+        _errorMessage != null
+            ? Text(_errorMessage!)
+            : const Text("User not found"),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Back'),
+        ),
+      ],
+    );
+  }
+
+  int _getRandomNumber(int from, int to) {
+    var random = Random();
+    return random.nextInt(to - from) + from;
+  }
+
+  void _handleFriendButtonClick() async {
+    if (_isFriend) {
+      await FirebaseUserController().removeFriend(widget.authorId);
+      setState(() {
+        _isFriend = false;
+      });
+    } else {
+      await FirebaseUserController().addFriend(widget.authorId);
+      setState(() {
+        _isFriend = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (!_hasData) {
+      return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: _userNotFound
+              ? _buildUserNotFound()
+              : const CircularProgressIndicator(),
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Major: ${widget.data.description}'),
+        title: Text('Major: ${_personData?.major ?? 'Unknown'}'),
       ),
       body: ListView(
         children: [
@@ -136,6 +285,7 @@ class _PersonState extends State<Person> {
                 _userName(),
                 _buildActionButton(),
                 _bioCard(),
+                _contacts(),
               ],
             ),
           ),
